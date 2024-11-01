f1277d2eb533a75bce7a78a229d9e16ba21fc559
codingRossi <seu_email@example.com>
2024-11-01 09:56:09 -0300

    change test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -2,30 +2,23 @@ import { AuthenticationError } from "@/domain/errors";
 import { LoadFacebookUserApi } from "@/data/contracts/apis";
 import { FacebookAuthenticationService } from "@/data/services"; 
 
-class LoadFacebookUserByTokenApiSpy implements LoadFacebookUserApi {
-    token?: string
-    result = undefined
-    callsCount = 0
-
-    async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
-        this.token = params.token;
-        this.callsCount++
-        return this.result
-    }
-}
     describe('FacebookAuthenticationService', () => {
         it('Should call LoadFacebookUserApi with correct params', async () => {
-            const LoadFacebookUserApi = new LoadFacebookUserByTokenApiSpy()
+            const LoadFacebookUserApi = {
+                loadUser: jest.fn()
+            }
             const sut = new FacebookAuthenticationService(LoadFacebookUserApi)
 
             await sut.perform({ token: 'any_token' })
-            expect(LoadFacebookUserApi.token).toBe('any_token')
-            expect(LoadFacebookUserApi.callsCount).toBe(1)
+            expect(LoadFacebookUserApi.loadUser).toHaveBeenCalledWith({token: 'any_token'})
+            expect(LoadFacebookUserApi.loadUser).toHaveBeenCalledTimes(1)
         })
 
         it('Should return AuthenticationError when LoadFacebookUserApi return undefined', async () => {
-            const LoadFacebookUserApi = new LoadFacebookUserByTokenApiSpy()
-            LoadFacebookUserApi.result = undefined
+            const LoadFacebookUserApi = {
+                loadUser: jest.fn()
+            }
+            LoadFacebookUserApi.loadUser.mockResolvedValueOnce(undefined)
             const sut = new FacebookAuthenticationService(LoadFacebookUserApi)
 
             const authResult = await sut.perform({ token: 'any_token' })
