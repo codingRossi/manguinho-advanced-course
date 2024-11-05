0d5c31eebb5473fafe112e8dfd2ea3c8a0a5ce08
codingRossi <seu_email@example.com>
2024-11-05 15:44:44 -0300

    ensure AxiosHttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -46,5 +46,12 @@ describe('AxiosHttpClient', () => {
 
             expect(result).toEqual('any_data')
         })
+
+        it('Should rethrow if get throws', async () => {
+            fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+            const promise = sut.get({url, params })
+
+            expect(promise).rejects.toThrow(new Error('http_error'))
+        })
     })
 })
