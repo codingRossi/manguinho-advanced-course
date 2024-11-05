c82053082744a52dd30f161f6458141b502002d4
codingRossi <seu_email@example.com>
2024-11-05 09:15:31 -0300

    ensure FacebookApi gets user info

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -2,19 +2,34 @@ import { LoadFacebookUserApi } from "@/data/contracts/apis"
 import { HttpGetClient } from "../http"
 
 export class FacebookApi {
-    private readonly basicUrl = 'https://graph.facebook.com'
+    private readonly baseUrl = 'https://graph.facebook.com'
     constructor(
         private readonly httpClient: HttpGetClient,
         private readonly clientId: string,
         private readonly clientSecret: string
     ) { }
     async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
-        await this.httpClient.get({
-            url: `${this.basicUrl}/oauth/access_token`,
+        const appToken = await this.httpClient.get({
+            url: `${this.baseUrl}/oauth/access_token`,
             params: {
                 client_id: this.clientId,
                 client_secret: this.clientSecret,
                 grant_type: 'client_credentials'
+            }
+        })
+        const debugToken = await this.httpClient.get({
+            url: `${this.baseUrl}/debug_token`,
+            params: {
+                access_token: appToken.access_token,
+                input_token: params.token,
+            }
+        })
+
+        await this.httpClient.get({
+            url: `${this.baseUrl}/${debugToken.data.user_id}`,
+            params: {
+                fields: ['id', 'name', 'email'].join(','),
+                access_token: params.token,
             }
         })
     }
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -11,26 +11,52 @@ describe('FacebookApi', () => {
     let httpClient: MockProxy<HttpGetClient>
 
     beforeAll(() => {
-        httpClient = mock()
         clientId = 'any_client_id'
         clientSecret = 'any_client_secret'
+        httpClient = mock()
     })
 
     beforeEach(() => {
-        const sut = new FacebookApi(httpClient, clientId, clientSecret)
+        httpClient.get
+            .mockResolvedValueOnce({ access_token: 'any_app_token' })
+            .mockResolvedValueOnce({ data: { user_id: 'any_user_id' } })
+        sut = new FacebookApi(httpClient, clientId, clientSecret)
+
     })
 
     it('Should get app token', async () => {
-
-        const sut = new FacebookApi(httpClient, clientId, clientSecret)
-
         await sut.loadUser({ token: 'any_client_token' })
+        
         expect(httpClient.get).toHaveBeenCalledWith({
             url: 'https://graph.facebook.com/oauth/access_token',
             params: {
                 client_id: clientId,
                 client_secret: clientSecret,
                 grant_type: 'client_credentials'
+            }
+        })
+    })
+
+    it('Should get debug token', async () => {
+        await sut.loadUser({ token: 'any_client_token' })
+
+        expect(httpClient.get).toHaveBeenCalledWith({
+            url: 'https://graph.facebook.com/debug_token',
+            params: {
+                access_token: "any_app_token",
+                input_token: "any_client_token",
+            }
+        })
+    })
+
+    it('Should get user info', async () => {
+        await sut.loadUser({ token: 'any_client_token' })
+
+        expect(httpClient.get).toHaveBeenCalledWith({
+            url: 'https://graph.facebook.com/any_user_id',
+            params: {
+                fields: 'id,name,email',
+                access_token: "any_client_token",
             }
         })
     })
