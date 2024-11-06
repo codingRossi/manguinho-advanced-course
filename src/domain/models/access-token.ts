dfb5289eb82e0c00099ecb4baeb54389c8d2b38d
codingRossi <seu_email@example.com>
2024-11-06 15:55:47 -0300

    ensure FacebookLoginController returns 200 authentication succeed

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,6 +1,6 @@
 
 export class AccessToken {
-    constructor(private readonly value: string) { }
+    constructor(readonly value: string) { }
 
     static get expirationInMs(): number {
         return 30 * 60 * 1000
